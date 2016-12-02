fd4cca91abb8d954c4067d2b89c56be2523040f0
Pamela Ocampo <pamela.ocampo@gmail.com>
2016-12-02 15:30:43 -0800

    Updates symlinks for bash-it

--- remove_dotfiles.sh
+++ remove_dotfiles.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-rm -r ~/.iterm
-rm -rf ~/.bash-it
-rm -rf ~/.inputrc
-rm -rf ~/.tmp
-rm ~/.bash_profile
-rm ~/.editorconfig
-rm ~/.gitattributes
-rm ~/.gitconfig
-rm ~/.gitignore
-rm ~/.vim
+rm -r ~/iterm
+rm -r ~/.bash-it
+rm -r ~/.inputrc
+rm -r ~/.tmp
+rm -r ~/.bash_profile
+rm -r ~/.editorconfig
+rm -r ~/.gitattributes
+rm -r ~/.gitconfig
+rm -r ~/.gitignore
+rm -r ~/.vim
 
 exit 0
