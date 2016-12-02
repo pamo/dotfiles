f9671c4dfe8a6c7af43fb3b62c31d816e91025f9
Pamela Ocampo <pamela.ocampo@gmail.com>
2016-12-01 16:55:45 -0800

    add bash it config to setup

--- setup.sh
+++ setup.sh
@@ -26,7 +26,7 @@ git submodule update
 # utils
 # Homebrew
 if [[ ! -x /usr/local/bin/brew ]] ; then
-				/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+                                /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 brew tap homebrew/versions
@@ -35,53 +35,54 @@ brew tap caskroom/versions
 brew tap caskroom/fonts
 
 brew_packages=(
-	autoconf
-	autojump
-	bash-completion
-	dos2unix
-	git
-	git-extras
-	gti
-	htop-osx
-	markdown
-	pkg-config
-	rbenv
-	rbenv-gemset
-	readline
-	ruby-build
-	ssh-copy-id
-	the_silver_searcher
-	tig
-	tree
-	trash
-	unrar
-	vim
-	wget
+        autoconf
+        autojump
+        bash-completion
+        dos2unix
+        git
+        git-extras
+        gti
+        htop-osx
+        markdown
+        pkg-config
+        rbenv
+        rbenv-gemset
+        readline
+        ruby-build
+        ssh-copy-id
+        the_silver_searcher
+        tig
+        tree
+        trash
+        unrar
+        vim
+        wget
 )
 brew install ${brew_packages[@]}
 
 # apps
 cask_packages=(
-	1password
-	alfred
-	alternote
-	bartender
-	dash
-	dropbox
-	fantastical
-	google-chrome
-	hipchat
-	iterm2-beta
-	lastfm
-	screenhero
-	slack
-	spectacle
-	spotify
+        1password
+        alfred
+        alternote
+        bartender
+        dash
+        dropbox
+        fantastical
+        google-chrome
+        hipchat
+        iterm2-beta
+        lastfm
+        screenhero
+        slack
+        spectacle
+        spotify
 )
 brew cask install ${cask_packages[@]}
 
 # Bash Config
 ./.bash-it/install.sh -s -n
+./.bash-it/configure.sh
 
 # Install iterm themes
 open iterm/*.itermcolors
