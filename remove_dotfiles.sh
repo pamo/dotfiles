3830da5531703a4f7bdcee7026c2c0faf7264362
Pamela Ocampo <pamela.ocampo@gmail.com>
2016-12-04 11:47:55 -0800

    Adds cask uninstall step

--- remove_dotfiles.sh
+++ remove_dotfiles.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/env bash
+cask_packages=(
+	adobe-photoshop-lightroom
+        1password
+        alfred
+        alternote
+        bartender
+        dash
+        dropbox
+        fantastical
+        flux
+	focus
+        google-chrome
+        hipchat
+        iterm2-beta
+        lastfm
+        polymail
+        screenhero
+        slack
+        spectacle
+        spotify
+)
+brew cask zap ${cask_packages[@]}
+
 rm -r ~/iterm
 rm -r ~/.bash-it
 rm -r ~/.inputrc
