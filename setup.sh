3425ef0e56a77af8ccb1dada5eb6d3d7d2215b02
Pamela Ocampo <pamela.ocampo@gmail.com>
2016-12-06 11:43:05 -0800

    Extracts brew setup into it's own shell script

--- setup.sh
+++ setup.sh
@@ -26,68 +26,8 @@ mkdir ~/.vim/tmp
 git submodule init
 git submodule update
 
-# utils
-# Homebrew
-if [[ ! -x /usr/local/bin/brew ]] ; then
-                                /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
-brew tap homebrew/versions
-brew tap caskroom/cask
-brew tap caskroom/versions
-brew tap caskroom/fonts
-
-brew_packages=(
-        autoconf
-        autojump
-        bash-completion
-        dos2unix
-        git
-        git-extras
-        gti
-        htop-osx
-        markdown
-        nodenv
-        pkg-config
-        rbenv
-        rbenv-gemset
-        readline
-        ruby-build
-        ssh-copy-id
-        the_silver_searcher
-        tig
-        tree
-        trash
-        unrar
-        vim
-        wget
-)
-brew install ${brew_packages[@]}
-
-# apps
-cask_packages=(
-	adobe-photoshop-lightroom
-        1password
-        alfred
-        alternote
-        bartender
-        dash
-        dropbox
-        fantastical
-        flux
-	focus
-        google-chrome
-        hipchat
-        iterm2-beta
-        lastfm
-        polymail
-        screenhero
-        slack
-        spectacle
-        spotify
-)
-brew cask install ${cask_packages[@]}
-ln -s "$HOME/Dropbox/Documents" ~
+echo "Installing brew and brew cask packages..."
+./brew_setup.sh
 
 # Bash Config
 ./.bash-it/install.sh -s -n
