aaa4854daddc154e2340742b46810169e1cf13d1
Pamela Ocampo <pamela.ocampo@gmail.com>
2016-12-10 17:16:55 -0800

    Echo setting being changed

--- osx_defaults.sh
+++ osx_defaults.sh
@@ -1,51 +1,75 @@
 #!/usr/bin/env bash
 
+# Ask for the administrator password upfront and run a keep-alive to update
+# existing `sudo` time stamp until script has finished
+sudo -v
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 echo "Setting write permissions on /Library/Preferences"
 sudo chmod 755 /Library/Preferences
 
-# Set Computer Name
-COMPUTER="PurpleNurple"
-echo "Setting computer name to $COMPUTER"
-sudo scutil --set ComputerName $COMPUTER && \
-sudo scutil --set HostName $COMPUTER && \
-sudo scutil --set LocalHostName $COMPUTER && \
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $COMPUTER
+echo ""
+echo "Would you like to set your computer name (System Preferences >> Sharing)?  (y/n)"
+read -r response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+  echo "What would you like it to be?"
+  read COMPUTER_NAME
+  sudo scutil --set ComputerName $COMPUTER_NAME
+  sudo scutil --set HostName $COMPUTER_NAME
+  sudo scutil --set LocalHostName $COMPUTER_NAME
+  sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $COMPUTER_NAME
+fi
 
 ###############################################################################
 # General UI/UX                                                               #
 ###############################################################################
 
-# Menu bar: show battery percentage
+echo ""
+echo "Menu bar: show battery percentage"
 defaults write com.apple.menuextra.battery ShowPercent -string "YES"
 
-# Set sidebar icon size to Small
+echo ""
+echo "Set sidebar icon size to Small"
 defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 1
 
-# Expand save panel by default
+echo ""
+echo "Expand save panel by default"
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 
-# Expand print panel by default
+echo ""
+echo "Expand print panel by default"
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 
-# Save to disk (not to iCloud) by default
+echo ""
+echo "Save to disk (not to iCloud) by default"
 defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
-# Disable the “Are you sure you want to open this application?” dialog
+echo ""
+echo "Disable the “Are you sure you want to open this application?” dialog"
 defaults write com.apple.LaunchServices LSQuarantine -bool false
 
-# Disable the Time Machine new disk requests dialog
+echo ""
+echo "Disable the Time Machine new disk requests dialog"
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
-# Menu bar: disable transparency
+echo ""
+echo "Menu bar: disable transparency"
 defaults write NSGlobalDomain AppleEnableMenuBarTransparency -bool false
 
-# Check for software updates daily, not just once per week
+echo ""
+echo "Check for software updates daily, not just once per week"
 defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 
-# Developer CrashReport dialog type
+echo ""
+echo "Disabling the sound effects on boot"
+sudo nvram SystemAudioVolume=" "
+
+echo ""
+echo "Setting Developer CrashReport dialog type"
 defaults write com.apple.CrashReporter DialogType developer
 
-# Reduce transparency
+echo ""
+echo "Reduce transparency"
 defaults write com.apple.universalaccess reduceTransparency -boolean true
 
 # Screenshots Default Location
@@ -58,153 +82,176 @@ defaults write com.apple.screencapture location $LOCATION && killall SystemUISer
 # Trackpad, mouse, keyboard, and input                                        #
 ###############################################################################
 
-# Don't automatically switch to a Space with open windows for and application when switching to it
+echo ""
+echo "Don't automatically switch to a Space with open windows for and application when switching to it"
 defaults write com.apple.dock workspaces-auto-swoosh -boolean NO
 
-# Don’t automatically rearrange Spaces based on most recent use
+echo ""
+echo "Don’t automatically rearrange Spaces based on most recent use"
 defaults write com.apple.dock mru-spaces -bool false
 
-# Keyboard: Key Repeat
-defaults write NSGlobalDomain ApplePressAndHoldEnabled -boolean true
+echo ""
+echo "Enable press-and-hold for keys and disable key repeat"
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool true
 
-# Set a blazingly fast keyboard repeat rate
+echo ""
+echo " Set a blazingly fast keyboard repeat rate"
 defaults write NSGlobalDomain KeyRepeat -int 0
 
-# Automatically illuminate built-in MacBook keyboard in low light
+echo ""
+echo "Automatically illuminate built-in MacBook keyboard in low light"
 defaults write com.apple.BezelServices kDim -bool true
 
-# Turn off keyboard illumination when computer is not used for 5 minutes
+echo ""
+echo "Turn off keyboard illumination when computer is not used for 5 minutes"
 defaults write com.apple.BezelServices kDimTime -int 300
 
-# Trackpad: enable tap to click for this user and for the login screen
+echo ""
+echo "Trackpad: enable tap to click for this user and for the login screen"
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
-# Fast tracking speed
+echo ""
+echo "Fast mouse tracking speed"
 defaults write -g com.apple.mouse.scaling 4.5
-# Trackpad: swipe between pages with three fingers
+
+echo ""
+echo "Trackpad: swipe between pages with three fingers"
 defaults write NSGlobalDomain AppleEnableSwipeNavigateWithScrolls -bool true
 defaults -currentHost write NSGlobalDomain com.apple.trackpad.threeFingerHorizSwipeGesture -int 1
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerHorizSwipeGesture -int 1
 
-# Enable full keyboard access for all controls
-# (e.g. enable Tab in modal dialogs)
+echo ""
+echo "Enabling full keyboard access for all controls (e.g. enable Tab in modal dialogs)"
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
-# Enable access for assistive devices
+echo ""
+echo "Enable access for assistive devices"
 echo -n 'a' | sudo tee /private/var/db/.AccessibilityAPIEnabled > /dev/null 2>&1
 sudo chmod 444 /private/var/db/.AccessibilityAPIEnabled
 
-# Use scroll gesture with the Ctrl (^) modifier key to zoom
+echo ""
+echo "Use scroll gesture with the Ctrl (^) modifier key to zoom"
 defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
 defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
-# Follow the keyboard focus while zoomed in
+
+echo ""
+echo "Follow the keyboard focus while zoomed in"
 defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
 
-# Function keys
+echo ""
+echo "Function keys"
 defaults write -g com.apple.keyboard.fnState -bool true
 
-# Increase sound quality for Bluetooth headphones/headsets
+echo ""
+echo "Increasing sound quality for Bluetooth headphones/headsets"
 defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
 
-# Speling Autocorrect
+echo ""
+echo "Enbabling Autocorrect"
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool true
 
 ###############################################################################
 # Screen Saver                                                                #
 ###############################################################################
 
-# Require password 5 minutes after sleep or screen saver begins
+echo ""
+echo "Require password 5 minutes after sleep or screen saver begins"
 defaults write com.apple.screensaver askForPasswordDelay -int 300
 
 ###############################################################################
 # Finder                                                                      #
 ###############################################################################
 
-# Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons
+echo ""
+echo "Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons"
 defaults write com.apple.finder QuitMenuItem -bool true
 
-# Finder: show hidden files by default
+echo ""
+echo "Finder: show hidden files by default"
 defaults write com.apple.Finder AppleShowAllFiles -bool true
 
-# Finder: set window title to full POSIX file path of current folder
+echo ""
+echo "Finder: set window title to full POSIX file path of current folder"
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
-# Finder: show all filename extensions
+echo ""
+echo "Finder: show all filename extensions"
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+echo ""
 
-# Finder: show path bar
+echo "Finder: show path bar"
 defaults write com.apple.finder ShowPathbar -bool true
 
-# Finder: show status bar
+echo ""
+echo "Finder: show status bar"
 defaults write com.apple.finder ShowStatusBar -bool true
 
-# Finder: allow text selection in Quick Look
+echo ""
+echo "Finder: allow text selection in Quick Look"
 defaults write com.apple.finder QLEnableTextSelection -bool true
 
-# When performing a search, search the current folder by default
+echo ""
+echo "When performing a search, search the current folder by default"
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
-# Disable the warning when changing a file extension
+echo ""
+echo "Disable the warning when changing a file extension"
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
-# Avoid creating .DS_Store files on network volumes
+echo ""
+echo "Avoid creating .DS_Store files on network volumes"
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
-# Automatically open a new Finder window when a volume is mounted
+echo ""
+echo "Automatically open a new Finder window when a volume is mounted"
 defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
 defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
 defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
 
-# Enable snap-to-grid for icons on the desktop and in other icon views
-/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-
-# Increase grid spacing for icons on the desktop and in other icon views
-/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:gridSpacing 70" ~/Library/Preferences/com.apple.finder.plist
-/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:gridSpacing 70" ~/Library/Preferences/com.apple.finder.plist
-/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:gridSpacing 70" ~/Library/Preferences/com.apple.finder.plist
-
-# Increase the size of icons on the desktop and in other icon views
-/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:iconSize 40" ~/Library/Preferences/com.apple.finder.plist
-/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:iconSize 40" ~/Library/Preferences/com.apple.finder.plist
-/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:iconSize 40" ~/Library/Preferences/com.apple.finder.plist
-
-# Disable the warning before emptying the Trash
+echo "Disable the warning before emptying the Trash"
 defaults write com.apple.finder WarnOnEmptyTrash -bool false
 
-# Use list view in all Finder windows by default
+echo ""
+echo "Use list view in all Finder windows by default"
 # Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 
-# Show the ~/Library folder
+echo ""
+echo "Show the ~/Library folder"
 chflags nohidden ~/Library
 
-# Disable window animations and Get Info animations
+echo ""
+echo "Disable window animations and Get Info animations"
 defaults write com.apple.finder DisableAllAnimations -bool true
 
-# Expand the following File Info panes:
-# “General”, “Open with”, and “Sharing & Permissions”
+echo ""
+echo "Expand the following File Info panes:"
+echo "“General”, “Open with”, and “Sharing & Permissions”"
 defaults write com.apple.finder FXInfoPanesExpanded -dict \
         General -bool true \
         OpenWith -bool true \
         Privileges -bool true
+
 ###############################################################################
 # Dock & hot corners                                                          #
 ###############################################################################
 
-# Set the icon size of Dock items
+echo ""
+echo "Set the icon size of Dock items"
 defaults write com.apple.dock tilesize -int 36
 
-# Don’t show Dashboard as a Space
+echo ""
+echo "Don’t show Dashboard as a Space"
 defaults write com.apple.dock "dashboard-in-overlay" -bool true
 
-# Make Dock icons of hidden applications translucent
+echo ""
+echo "Make Dock icons of hidden applications translucent"
 defaults write com.apple.dock showhidden -bool true
 
-# Hot corners
+echo ""
+echo "Hot corners"
 # Possible values:
 #  0: no-op
 #  2: Mission Control
@@ -217,18 +264,22 @@ defaults write com.apple.dock showhidden -bool true
 # 11: Launchpad
 # 12: Notification Center
 
-# Top right screen corner → Show Desktop
+echo ""
+echo "Top right screen corner → Show Desktop"
 defaults write com.apple.dock wvous-tr-corner -int 4
 defaults write com.apple.dock wvous-tr-modifier -int 0
 
-# Bottom right screen corner → Show Screen Saver
+echo ""
+echo "Bottom right screen corner → Show Screen Saver"
 defaults write com.apple.dock wvous-br-corner -int 5
 defaults write com.apple.dock wvous-br-modifier -int 0
 
-# Minimize windows into their application’s icon
+echo ""
+echo "Minimize windows into their application’s icon"
 defaults write com.apple.dock minimize-to-application -bool false
 
-# Wipe all (default) app icons from the Dock
+echo ""
+echo "Wipe all (default) app icons from the Dock"
 # This is only really useful when setting up a new Mac, or if you don’t use
 # the Dock to launch apps.
 defaults write com.apple.dock persistent-apps -array
@@ -237,44 +288,55 @@ defaults write com.apple.dock persistent-apps -array
 # Safari & WebKit                                                             #
 ###############################################################################
 
-# Privacy: don’t send search queries to Apple
+echo ""
+echo "Privacy: don’t send search queries to Apple"
 defaults write com.apple.Safari UniversalSearchEnabled -bool false
 defaults write com.apple.Safari SuppressSearchSuggestions -bool true
 
-# Set Safari’s home page to nothing
+echo ""
+echo "Set Safari’s home page to nothing"
 defaults write com.apple.Safari HomePage -string "about:blank"
 
-# Hide Safari’s bookmarks bar by default
+echo ""
+echo "Hide Safari’s bookmarks bar by default"
 defaults write com.apple.Safari ShowFavoritesBar -bool false
 
-# Disable Safari’s thumbnail cache for History and Top Sites
+echo ""
+echo "Disable Safari’s thumbnail cache for History and Top Sites"
 defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
 
-# Enable Safari’s debug menu
+echo ""
+echo "Enable Safari’s debug menu"
 defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 
-# Remove useless icons from Safari’s bookmarks bar
+echo ""
+echo "Remove useless icons from Safari’s bookmarks bar"
 defaults write com.apple.Safari ProxiesInBookmarksBar "()"
 
-# Enable the Develop menu and the Web Inspector in Safari
+echo ""
+echo "Enable the Develop menu and the Web Inspector in Safari"
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 
-# Add a context menu item for showing the Web Inspector in web views
+echo ""
+echo "Add a context menu item for showing the Web Inspector in web views"
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
-# Prevent Safari from opening "safe" files automatically after downloading
+echo ""
+echo "Prevent Safari from opening "safe" files automatically after downloading"
 defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
 
 ###############################################################################
 # Activity Monitor                                                            #
 ###############################################################################
 
-# Show the main window when launching Activity Monitor
+echo ""
+echo "Show the main window when launching Activity Monitor"
 defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
 
-# Show all processes in Activity Monitor
+echo ""
+echo "Show all processes in Activity Monitor"
 defaults write com.apple.ActivityMonitor ShowCategory -int 0
 
 
@@ -282,10 +344,12 @@ defaults write com.apple.ActivityMonitor ShowCategory -int 0
 # Mac App Store                                                               #
 ###############################################################################
 
-# Enable the WebKit Developer Tools in the Mac App Store
+echo ""
+echo "Enable the WebKit Developer Tools in the Mac App Store"
 defaults write com.apple.appstore WebKitDeveloperExtras -bool true
 
-# Enable Debug Menu in the Mac App Store and Reminders app
+echo ""
+echo "Enable Debug Menu in the Mac App Store and Reminders app"
 defaults write com.apple.appstore ShowDebugMenu -bool true
 defaults write com.apple.reminders RemindersDebugMenu -boolean true
 
@@ -302,4 +366,3 @@ osascript -e 'tell application "System Events" to make new login item at end wit
 osascript -e 'tell application "System Events" to make new login item at end with properties {path:"/Applications/Slack.app", name:"Slack", hidden:true}'
 osascript -e 'tell application "System Events" to make new login item at end with properties {path:"/Applications/Spectacle.app", name:"Spectacle", hidden:true}'
 echo "Done. Note that some of these changes require a logout/restart to take effect."
-
