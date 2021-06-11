e20ffe240d46c2c7f9ed6a09da37fcd47e349ac9
Pamela Ocampo <pamela.ocampo@gmail.com>
2021-06-10 18:58:13 -0700

    Remove bash-it config

--- setup.sh
+++ setup.sh
@@ -10,13 +10,12 @@ echo "Setting OSX Defaults..."
 ./osx_defaults.sh
 
 mkdir ~/.tmp
-mkdir ~/Development
+mkdir ~/dev
 ln -sfn "$(pwd -P)" ~/
 ln -sfn "$(pwd)/.bash_profile" ~
 ln -sfn "$(pwd)/.gitconfig" ~
 ln -sfn "$(pwd)/.gitignore" ~
 ln -sfn "$(pwd)/.gitattributes" ~
-ln -sfn "$(pwd)/.bash-it" ~
 ln -sfn "$(pwd)/.inputrc" ~
 ln -sfn "$(pwd)/.gemrc" ~
 ln -sfn "$(pwd)/.npmrc" ~
@@ -35,11 +34,6 @@ git submodule update --remote --rebase
 
 echo "Installing brew and brew cask packages..."
 ./brew_setup.sh
-
-# Bash Config
-echo "Configuring bash-it"
-./.bash-it/install.sh -s -n
-./.bash-it/configure.sh
 
 # Custom Sounds
 cp -r "$(pwd)/Sounds/" ~/Library/Sounds/
